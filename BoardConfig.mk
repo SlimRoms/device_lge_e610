02bb287091b251a54db941fc2f86704c9e02d5a4
RonGokhale <cips173@gmail.com>
2013-06-05 17:19:20 +0530

    e610: move to common
    Change-Id: I47d49d2844e26a5ead7511963c760b2f3c92e238

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,5 @@ TARGET_KERNEL_CONFIG := cyanogenmod_m4_defconfig
 
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := e610
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/e610/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/lge/e610/bluetooth/vnd_m4.txt
 
 BOARD_CUSTOM_GRAPHICS := ../../../device/lge/e610/recovery/graphics.c
