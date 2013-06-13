56c6e6bae9c47f3fd30f3c62ecf00f6c0c33e428
RonGokhale <cips173@gmail.com>
2013-06-13 16:23:38 +0530

    e610: update fingerprint from v20c
    Change-Id: Icdc5b41a6b158618beef813f397a8b55007221c0

--- cm.mk
+++ cm.mk
@@ -1,26 +1,30 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 480
-TARGET_SCREEN_WIDTH := 320
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/e610/e610.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := e610
 PRODUCT_NAME := cm_e610
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-E610
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=m4_open_eu \
+    BUILD_DISPLAY_ID=JDQ39E \
+    BUILD_FINGERPRINT="lge/m4_open_eu/m4:4.1.2/JZO54K/E61020c-EUR-XX.1367460723:user/release-keys" \
+    PRIVATE_BUILD_DESC="m4_open_eu-user 4.1.2 JZO54K E61020c-EUR-XX.1367460723 release-keys"
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := OptimusL5
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := e610
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m4_open_eu BUILD_FINGERPRINT=lge/m4_open_eu/m4:4.0.3/IML74K/lge610-V10a.20121008.170544:user/release-keys PRIVATE_BUILD_DESC="m4_open_eu-user 4.0.3 IML74K lge610-V10a.20121008.170544 release-keys"
-
-# Enable Torch
-#PRODUCT_PACKAGES += Torch
